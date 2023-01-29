9a681e779fd32f8be290888589a20f72309aa833
shamimsikder <shamimsikder138@gmail.com>
2023-01-29 14:58:26 +0600

    JS Array

---
+++ Milestone 4/array.js
@@ -0,0 +1,27 @@
+var arr = [100,54,23,89,12]
+
+console.log(arr);
+console.log(arr.length);
+console.log(arr[1]);
+
+arr[0] = 99;
+
+console.log(arr);
+
+console.log(arr.indexOf(23));
+
+arr.push(420)
+
+console.log(arr);
+
+arr.pop()
+
+console.log(arr);
+
+arr.sort()
+
+console.log(arr);
+
+arr.reverse()
+
+console.log(arr);
