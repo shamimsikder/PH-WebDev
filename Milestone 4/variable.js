1b090264cd8a88aa4a1b14c92b9d679ea23091bc
shamimsikder <shamimsikder138@gmail.com>
2023-01-29 13:54:38 +0600

    JS Variable

---
+++ Milestone 4/variable.js
@@ -0,0 +1,14 @@
+var x = 10;
+var y = 20;
+var string1 = "Barking Dogs";
+var string2 = " Seldom Bite";
+
+var a = 3.6;
+
+console.log(x);
+console.log(x + y);
+console.log(a);
+console.log(string1);
+console.log(string1 + string2);
+
+console.log(x < y);
