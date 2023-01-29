e5080d4242954c20c6e208afbbe998623acd07d0
shamimsikder <shamimsikder138@gmail.com>
2023-01-29 16:05:21 +0600

    JS Conditional Statement

---
+++ Milestone 4/conditional_statement.js
@@ -0,0 +1,22 @@
+var x = 19;
+
+if (x >= 18){
+    console.log("You can buy alcohol");}
+else{
+    console.log("Too young to buy alcohol");
+}
+
+var math = 45;
+var english = 68;
+
+if(math >= 80 && english >= 80){
+    console.log("A+");
+}
+
+else if(math < 80 && math > 60 && english > 60 && english < 80){
+    console.log("A");
+}
+
+else{
+    console.log("Fail")
+}
