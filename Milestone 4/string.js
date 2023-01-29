48ab714243bb004b278abfd126862dce311031b4
shamimsikder <shamimsikder138@gmail.com>
2023-01-29 14:33:59 +0600

    JS String

---
+++ Milestone 4/string.js
@@ -0,0 +1,12 @@
+var str = "Hello JS";
+var fruits = "Apple, Banana, Kiwi";
+
+console.log(str.length);
+
+console.log(typeof(fruits)); 
+
+console.log(fruits.slice(7,13)); 
+
+console.log(str.replace("Hello","Hola"));
+
+console.log(fruits.toUpperCase());
