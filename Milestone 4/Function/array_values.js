d6e12515d4f510d6ae4a15b3c724fa2418855c98
shamimsikder <shamimsikder138@gmail.com>
2023-01-31 00:20:10 +0600

    Function Practice

---
+++ Milestone 4/Function/array_values.js
@@ -0,0 +1,11 @@
+function printArray(arr){
+
+    for(i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+
+}
+
+let a = [1,6,3,89];
+
+printArray(a);
---
+++ Milestone 4/Function/max_number.js
@@ -0,0 +1,9 @@
+function maximum(x,y){
+
+    if(x > y) return x;
+
+    else return y;
+
+}
+
+console.log(maximum(3,12));
