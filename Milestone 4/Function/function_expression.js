3c1dea91c452b868cef30a4c2a2538051d381ed0
shamimsikder <shamimsikder138@gmail.com>
2023-01-31 23:55:45 +0600

    Function Expression

---
+++ Milestone 4/Function/function_expression.js
@@ -0,0 +1,17 @@
+let run =  function(){
+    console.log('run');
+};
+
+run()
+
+let walk =  function(){
+    console.log('Walk');
+};
+
+walk()
+
+let sum = function(a,b){
+    return a + b;
+};
+
+console.log(sum(8,9));
