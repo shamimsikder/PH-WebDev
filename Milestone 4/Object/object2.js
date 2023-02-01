f6f316be9846fd40be3f0819aad8fc2081a3300f
shamimsikder <shamimsikder138@gmail.com>
2023-02-01 20:00:49 +0600

    object

---
+++ Milestone 4/Object/object2.js
@@ -0,0 +1,23 @@
+let student = {
+    fname: "Shamim",
+    lname: "Sikder",
+    age: 25,
+    id: 19115
+}
+
+console.log(student);
+console.log(student.age);
+
+student.id = 1911512;
+
+console.log(student);
+
+let shoppingCart = {
+    books: 3,
+    sunglass: 1,
+    keyboard: 1,
+    mouse: 1,
+    cap: 2
+}
+
+console.log(shoppingCart)
