390caf6bab4aee34a07f41d77da580c5bcefb6ce
shamimsikder <shamimsikder138@gmail.com>
2023-02-01 09:05:32 +0600

    Function Expression

---
+++ Milestone 4/Function/function_expression2.js
@@ -0,0 +1,18 @@
+
+let res = function largestNumber(arr){
+
+    let n;
+
+    for(i=1; i<arr.length; i++){
+
+        if(arr[0] < arr[i]) return n = arr[i];
+
+        else return arr[0];
+
+    }
+
+}
+
+let arr = [2,69,56,3,2,45];
+
+console.log(res(arr));
