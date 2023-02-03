8ed25ed9f545112c65cb871d3dfda800eb012238
shamimsikder <shamimsikder138@gmail.com>
2023-02-03 21:06:02 +0600

    Built-in Function of String

---
+++ Milestone 4/Function/builtInFunction.js
@@ -0,0 +1,14 @@
+let str = "Party Rock Anthem";
+
+console.log(str.charAt(4));
+
+console.log(str.indexOf("Rock"));
+
+console.log(str.split(''));
+
+console.log(str.substr(6,4));
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+
