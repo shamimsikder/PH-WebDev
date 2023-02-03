7ac7b70ef65acedd2766fafaa0471ed7f51d5d72
shamimsikder <shamimsikder138@gmail.com>
2023-02-03 19:30:01 +0600

    Blog Post Object

---
+++ Milestone 4/Object/blog_post_object.js
@@ -0,0 +1,13 @@
+let blog = {
+
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    views: 10,
+    comments: [
+        {author: 'a', body: 'b'},
+        {author: 'c', body: 'd'}
+    ],
+};
+
+console.log(blog)
