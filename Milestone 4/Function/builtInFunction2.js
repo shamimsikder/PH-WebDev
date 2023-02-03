15ff4c9994124d0baccdb6688a4d8ab181b6eaf9
shamimsikder <shamimsikder138@gmail.com>
2023-02-03 21:33:42 +0600

    Built-in Function of Array

---
+++ Milestone 4/Function/builtInFunction2.js
@@ -0,0 +1,17 @@
+// Built-in Function of Array
+
+let arr = [45,22,65,44,21,76,12,90];
+
+console.log(arr);
+
+console.log(arr.reverse());
+
+console.log(arr.sort());
+
+console.log(Math.max(...arr));
+
+console.log(Math.min(...arr));
+
+console.log(arr.push(5),arr);
+
+console.log(arr.pop(),arr);
