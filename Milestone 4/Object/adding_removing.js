1579caee499905676b8ce4d6f0a13afc84dd91a1
shamimsikder <shamimsikder138@gmail.com>
2023-02-05 09:58:44 +0600

    Adding and Removing

---
+++ Milestone 4/Object/adding_removing.js
@@ -0,0 +1,16 @@
+let object = {
+
+    cap: 3,
+    sunglass: 2,
+
+};
+
+console.log(object);
+
+object.shirt = 4;
+object.tshirt=3;
+
+console.log(object);
+
+delete object.sunglass
+console.log(object);
