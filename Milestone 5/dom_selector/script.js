69ad3ef76f3b842a372c5ae5bdfc7205f3b26b5a
shamimsikder <shamimsikder138@gmail.com>
2023-02-09 13:22:36 +0600

    getElementsByClassName()

--- Milestone 5/dom_selector/script.js
+++ Milestone 5/dom_selector/script.js
@@ -9,8 +9,18 @@ for(let i of header) console.log(i);*/
 
 //getElementById()
 
-const heading = document.getElementById('h1')
+const heading = document.getElementById('h1');
 
-console.log(heading.innerText)
-console.log(heading.innerHTML)
-console.log(heading.innerHTML = 'Document Object Model')
+console.log(heading.innerText);
+console.log(heading.innerHTML);
+console.log(heading.innerHTML = 'Document Object Model');
+
+//getElementsByClass
+
+const items = document.getElementsByClassName('item');
+
+console.log(items);
+
+for(let item of items) console.log(item);
+for(let item of items) console.log(item.innerHTML);
+
