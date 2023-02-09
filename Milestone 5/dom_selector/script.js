5c80926a4ed7c7f067b211a243feb268f89bd1d1
shamimsikder <shamimsikder138@gmail.com>
2023-02-09 12:49:24 +0600

    getElementsByTagName()

---
+++ Milestone 5/dom_selector/script.js
@@ -0,0 +1,8 @@
+// getElementsByTagName()
+
+const header = document.getElementsByTagName('h1');
+
+console.log(header)
+
+for(let i of header) console.log(i.innerText)
+for(let i of header) console.log(i)
