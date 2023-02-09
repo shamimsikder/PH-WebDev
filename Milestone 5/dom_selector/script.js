b0c9b7c3d193dd7b740a34bd04f4831dca5a3393
shamimsikder <shamimsikder138@gmail.com>
2023-02-09 16:17:38 +0600

    querySelectorAll()

--- Milestone 5/dom_selector/script.js
+++ Milestone 5/dom_selector/script.js
@@ -5,7 +5,7 @@
 console.log(header);
 
 for(let i of header) console.log(i.innerText);
-for(let i of header) console.log(i);*/
+for(let i of header) console.log(i);
 
 //getElementById()
 
@@ -22,5 +22,20 @@ const items = document.getElementsByClassName('item');
 console.log(items);
 
 for(let item of items) console.log(item);
-for(let item of items) console.log(item.innerHTML);
+for(let item of items) console.log(item.innerHTML);*/
+
+//querySelectorAll()
+
+const list = document.querySelectorAll('li')
+
+console.log(list)
+
+for(let i of list) console.log(i.innerText)
+
+const fruits_list = document.querySelectorAll('.fruits-container li')
+
+console.log(fruits_list)
+
+for(let i of fruits_list) console.log(i.innerText)
+
 
