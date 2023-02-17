faf226f4c215228d74fe24998f5013df6de6febc
shamimsikder <shamimsikder138@gmail.com>
2023-02-17 11:02:22 +0600

    Get Value JS

---
+++ Milestone 5/money-master/js/get-value.js
@@ -0,0 +1,10 @@
+function getInput(input){
+
+    const inputField = document.getElementById(input);
+
+    inputString = inputField.value;
+    inputValue = parent(inputString);
+
+    return inputValue;
+
+}
