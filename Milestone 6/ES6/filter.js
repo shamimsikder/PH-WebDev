bbb00d1b228c2b6c6b920efcedf89cac8aba6886
shamimsikder <shamimsikder138@gmail.com>
2023-02-21 20:10:02 +0600

    Filter

---
+++ Milestone 6/ES6/filter.js
@@ -0,0 +1,9 @@
+const arr = [2,4,5,7,1];
+
+const array = arr.filter(n => n % 2 === 0)
+const array2 = arr.filter(n => n % 2)
+const array3 = arr.filter(n => n < 5)
+
+console.log(array)
+console.log(array2)
+console.log(array3)
