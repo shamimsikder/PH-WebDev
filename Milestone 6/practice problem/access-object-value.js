1c6b8f7cf04bdf1abcfb3c2cf17b4adbf49488b6
shamimsikder <shamimsikder138@gmail.com>
2023-02-21 22:59:59 +0600

    Access Object Value

---
+++ Milestone 6/practice problem/access-object-value.js
@@ -0,0 +1,30 @@
+// Problem - 1
+
+const student = {
+
+    name: 'abcd',
+    age: 28
+
+}
+
+// Problem - 2
+console.log(student.age)
+
+const data = {
+
+    location: [
+
+        {
+
+            latitude: '34.5, 37.8',
+            longitude: '98.77, 58.7',
+            city: 'Dhaka',
+            country: 'Bangladesh'
+
+        }
+
+    ]
+
+};
+
+console.log(data.location[0].city)
