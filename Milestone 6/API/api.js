4084c0edb558d0592a4303d110f5ec67bafda963
shamimsikder <shamimsikder138@gmail.com>
2023-02-23 12:49:58 +0600

    Display Data From API

--- Milestone 6/API/api.js
+++ Milestone 6/API/api.js
@@ -1,3 +1,18 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+document.getElementById('load-btn').addEventListener('click', function(){
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => displayData(json))
+
+
+    function displayData(data){
+
+        const dataShow = document.getElementById('load-data')
+        
+        dataShow.innerText = data.title
+
+    }
+
+})
+
+
