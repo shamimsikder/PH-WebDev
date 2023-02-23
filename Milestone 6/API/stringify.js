87391c96d3a30ecd7413275e00b2abf67f26433a
shamimsikder <shamimsikder138@gmail.com>
2023-02-23 12:56:06 +0600

    Object to JSON

---
+++ Milestone 6/API/stringify.js
@@ -0,0 +1,13 @@
+const obj = {
+
+    name: "abc",
+    age: 30,
+    job: "xyz"
+
+}
+
+// Convert Object to JSON( JavaScript Object Notation)
+const toJSON = JSON.stringify(obj)
+
+console.log(obj)
+console.log(toJSON)
