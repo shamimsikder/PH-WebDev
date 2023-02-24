e17c5a484fde614237e671592f2897b4d3e1b0eb
shamimsikder <shamimsikder138@gmail.com>
2023-02-25 01:27:22 +0600

    modal added

--- Milestone 6/More API/js/mealdb.js
+++ Milestone 6/More API/js/mealdb.js
@@ -21,12 +21,17 @@ const displayMeal = meals => {
         mealDiv.innerHTML = `
         
         <div class="card h-100">
-        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+
+            <button onclick = "loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+            Meal Details
+            </button>
+
         </div>
-      </div>
         
         `;
 
@@ -44,4 +49,25 @@ const searchMeals = () => {
 
 }
 
-loadMeal()
+const loadMealDetail = idMeal => {
+
+    const url =  `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+
+}
+
+const displayMealDetails = meal => {
+
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+    `
+
+}
+
+loadMealDetail()
+loadMeal('fish')
