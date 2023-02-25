0719abead0bdd2e43e106cecae92fdd7cc7e709e
shamimsikder <shamimsikder138@gmail.com>
2023-02-25 12:42:01 +0600

    Async & Error Handling

--- Milestone 6/More API/js/countries.js
+++ Milestone 6/More API/js/countries.js
@@ -3,6 +3,30 @@ const loadData = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => displayData(data))
+        .catch(error => {
+
+            console.log(error)
+
+        })
+
+}
+
+const loadData2 = async() => {
+
+    try{
+
+        const res = await fetch('https://restcountries.com/v3.1/all')
+        const data = await res.json()
+    
+        displayData(data)
+
+    }
+
+    catch(error){
+
+        console.log(error)
+
+    }
 
 }
 
