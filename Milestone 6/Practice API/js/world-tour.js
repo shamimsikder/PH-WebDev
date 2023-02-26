9fcd9e381ec33b1ab382746305561e686cf97460
shamimsikder <shamimsikder138@gmail.com>
2023-02-27 00:20:41 +0600

    World Tour API Complete

--- Milestone 6/Practice API/js/world-tour.js
+++ Milestone 6/Practice API/js/world-tour.js
@@ -1,42 +1,79 @@
-const loadAllData = () => {
-
-    fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => displayAllData(data))
-
-}
-
-const displayAllData = (countries) => {
-
-    const container = document.getElementById('countries-info')
-
-    countries.slice(1, 9).forEach((country) => {
-
-        const div = document.createElement('div')
-
-       // div.classList.add(`card w-96 bg-base-100 shadow-xl`)
-
-        div.innerHTML = `
-        <div class="card w-96 h-96 bg-base-100 shadow-xl">
-            <figure class="px-10 pt-10">
-                <img src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
-            </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">${country.name.common}</h2>
-                <p>Population: ${country.population}</p>
-                <div class="card-actions">
-                
+const loadMyData = async () => {
+    const res = await fetch("https://restcountries.com/v3.1/all");
+    const data = await res.json();
+    console.log(data);
+    showAllData(data.slice(0, 5));
+};
+  
+loadMyData();
+  
+const showAllData = (countries) => {
+   
+    const countryContainer = document.getElementById("countries-info");
+    countryContainer.innerHTML = "";
+   
+    countries.forEach((country) => {
+            
+    const div = document.createElement("div");
+    
+    div.innerHTML = `
+        <div class="card w-full h-96 bg-base-100 shadow-2xl">
+        <figure class="px-10 pt-10">
+            <img src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">${country.name.common}</h2>
+            <p>Population: ${country.population}</p>
+            <p>Currency: ${Object.keys(country.currencies)[0]}</p>
+            <div class="card-actions">
+            <label onclick="showSingleCountry('${
+                country.cca2
+            }')" class="btn btn-primary" for="my-modal-3" class="btn">Show Details</label>
             </div>
         </div>
-
-        
+        </div>
         `;
-
-        container.appendChild(div)
-
+        countryContainer.appendChild(div);
     })
+}
+  
+const showAllDataTogether = () => {
+
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => showAllData(data))
 
 }
+  
+const showSingleCountry = (id) => {
+    
+    const URL = `https://restcountries.com/v3.1/alpha/${id}`;
+    
+    fetch(URL)
+        .then((res) => res.json())
+        .then((data) => showSingleCountryDataModal(data[0]));
 
+}
+  
+ 
+const showSingleCountryDataModal = (value) => {
+    
+    const container = document.getElementById("modal-info");
+    const div = document.createElement("div");
+    
+    div.classList.add("modal");
+    div.innerHTML = `
+    
+        <div class="modal-box relative">
+            
+            <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <img src="${value.flags.png}" alt="">
+            <h3 class="text-lg font-bold">${value.name.common}</h3>
+            <p class="py-4">Population : ${value.population}</p>
+        
+        </div>
+    `;
 
-loadAllData()
+    container.appendChild(div);
+
+}
