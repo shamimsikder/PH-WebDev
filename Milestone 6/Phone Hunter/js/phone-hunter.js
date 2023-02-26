3a1977a680cf9b9dc5f22d4f7b2193ee163931ce
shamimsikder <shamimsikder138@gmail.com>
2023-02-26 11:36:49 +0600

    load and show phone data

---
+++ Milestone 6/Phone Hunter/js/phone-hunter.js
@@ -0,0 +1,50 @@
+const loadData = async() => {
+
+    try{
+
+        const URL = "https://openapi.programming-hero.com/api/phones?search=iphone"
+
+        const res = await fetch(URL)
+        const data = await res.json()
+
+        showData(data.data)
+
+    }
+
+    catch(err){
+
+        console.log(err)
+
+    }
+
+}
+
+const showData = (phones) => {
+
+    const container = document.getElementById('data-container')
+
+    phones.slice(0, 10).forEach((phone) => {
+        console.log(phone.image)
+        const div = document.createElement('div')
+
+        div.innerHTML =  `
+        
+        <div class="card w-96 h-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">${phone.phone_name}</h2>
+                <div class="card-actions">
+                
+            </div>
+        </div>
+        `;
+
+        container.appendChild(div)
+
+    })
+
+}
+
+loadData()
