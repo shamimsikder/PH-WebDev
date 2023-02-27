5d56b02629e191831e3b228b7284c33c204f1923
shamimsikder <shamimsikder138@gmail.com>
2023-02-27 14:42:03 +0600

    load and show data

--- Milestone 6/Practice API/js/api-practice-1.js
+++ Milestone 6/Practice API/js/api-practice-1.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+// Convert Object to JSON
+/*const fs = require('fs')
 
 const person = {
     found: 2,
@@ -32,6 +33,7 @@ const person = {
   };
   
   const json = JSON.stringify(person);
+
   fs.writeFile('person.json', json, (err) => {
     if (err) {
       console.error(err);
@@ -39,3 +41,43 @@ const person = {
       console.log('File saved!');
     }
   });
+*/
+  // Problem-1
+
+const loadData = async() => {
+
+    const res = await fetch('person.json');
+    const data = await res.json()
+    showAllData(data);
+
+}
+
+const showAllData = (datas) => {
+    
+    const listTitle = document.getElementById('list');
+    const container = document.getElementById('data');
+  
+    listTitle.innerText = datas.message;
+  
+    datas.result.forEach((data) => {
+        
+        const div = document.createElement('div');
+        div.classList.add('bg-slate-100',  'shadow-xl');
+  
+        div.innerHTML = `
+            
+            <p class="border-2 border-slate-300 px-3 py-3 mb-5">Name: ${data.name.common}</p>
+            <p class="px-3 py-3">Age: ${data.age}</p>
+            <p class="px-3 py-3">Address: ${data.address.street}, House ${data.address.house}</p>
+      
+        `;
+  
+      container.appendChild(div);
+    
+    });
+
+}
+  
+  
+
+loadData()
