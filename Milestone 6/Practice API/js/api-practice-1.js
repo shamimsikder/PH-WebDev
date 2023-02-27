5aa4d7b9e9735e7e641b6094d5e94d15e65f5e47
shamimsikder <shamimsikder138@gmail.com>
2023-02-27 12:54:52 +0600

    conver to json and save to json file

---
+++ Milestone 6/Practice API/js/api-practice-1.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+
+const person = {
+    found: 2,
+    message: "Found 2 persons",
+    result: [
+      {
+        name: {
+          common: "John",
+          fullName: ["John", "Doe"]
+        },
+        age: 32,
+        isMale: false,
+        address: {
+          street: "13/A St Joseph",
+          house: 10,
+        },
+      },
+      {
+        name: {
+          common: "Humayoun",
+          fullName: ["Humayoun", "Kabir"]
+        },
+        age: 33,
+        isMale: false,
+        address: {
+          street: "13/A St Lucia",
+          house: 11,
+        },
+      },
+    ]
+  };
+  
+  const json = JSON.stringify(person);
+  fs.writeFile('person.json', json, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('File saved!');
+    }
+  });
