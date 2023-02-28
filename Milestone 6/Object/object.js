1b6ae887bdefe6866408df928294ae27dcfa712f
shamimsikder <shamimsikder138@gmail.com>
2023-03-01 01:40:27 +0600

    Object Literals

---
+++ Milestone 6/Object/object.js
@@ -0,0 +1,20 @@
+// Object Literals
+
+const object = {}
+
+object.name = "kuddus"
+object.age = 69
+object.work = function(){console.log("Web Dev")}
+
+console.log(object)
+
+const object2 = {
+    
+    name: "kuddus",
+    age: 69,
+    work(){
+        console.log("Web Dev")
+    }
+    
+}
+console.log(object2)
