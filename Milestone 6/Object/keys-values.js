69d2c4c772904eb7fc276e0d12df1389b60d90ec
shamimsikder <shamimsikder138@gmail.com>
2023-03-01 12:12:03 +0600

    Keys & Values

---
+++ Milestone 6/Object/keys-values.js
@@ -0,0 +1,14 @@
+const bottle = {
+
+    color: "green",
+    price: 30,
+    isCleaned: true,
+    capacity: 1
+
+}
+
+console.log(Object.keys(bottle))
+console.log(Object.values(bottle))
+
+
+console.log(Object.entries(bottle))
