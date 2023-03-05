ef89a46f42618298ca957e5325dfc66b10353d95
shamimsikder <shamimsikder138@gmail.com>
2023-03-05 11:41:54 +0600

    Fetch & Show News Details In Modal

--- Milestone 6/News Portal/js/script.js
+++ Milestone 6/News Portal/js/script.js
@@ -123,4 +123,57 @@ const showAllNews = (data, category_name) => {
 
 }
 
+const fetchNewsDetail = (news_id) => {
+
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => showNewsDetails(data.data[0]))
+
+}
+
+const showNewsDetails = (newsDetails) => {
+
+    const {_id, image_url, title, details, author, total_view, others_info} = newsDetails
+
+    document.getElementById("modal-body").innerHTML = `
+        <div class= "card mb-3">
+            <div class="row g-0">
+                <div class="col-md-12">
+                    <img src=${image_url} class="img-fluid rounded-start" alt="..." />
+                </div>
+                <div class="col-md-12 d-flex flex-column">
+                    <div class="card-body">
+                        <h5 class="card-title">${title} <span class="badge text-bg-warning">${others_info.is_trending ? "Trending" : "Not trending"}</span></h5>
+                        <p class="card-text">${details}</p>
+            
+                    </div>
+                    <div class="card-footer border-0 bg-body d-flex justify-content-between">
+                        <div class="d-flex gap-2">
+                            <img src=${author.img} class="img-fluid rounded-circle" alt="..." height="40" width="40"/>
+                        <div>
+                        <p class="m-0 p-0">${author.name ? author.name : "Not available"}</p>
+                        <p class="m-0 p-0">${author.published_date}</p>
+                    </div>
+            
+                </div>
+            
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-eye"></i>
+                    
+                    <p class="m-0 p-0">${total_view}</p>
+                </div>
+                <div>
+                    <i class="fas fa-star"></i>
+                
+                </div>
+            
+            </div>
+        </div>
+    </div>
+    </div>`
+
+}
+
 loadCategoriesData()
