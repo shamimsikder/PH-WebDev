593b1147278a6ee5d6bec2b5b231ad482770cf19
shamimsikder <shamimsikder138@gmail.com>
2023-03-12 01:38:43 +0600

    LocalStorage

---
+++ Milestone 7/BOM Practice/localStorage3.js
@@ -0,0 +1,43 @@
+document.getElementById('send-name').addEventListener('click', function(){
+
+    const name = document.getElementById('input-name').value
+
+    localStorage.setItem("Name", name)
+
+})
+
+document.getElementById('send-email').addEventListener('click', function(){
+
+    const email = document.getElementById('input-email').value
+
+    localStorage.setItem("Email", email)
+
+})
+
+document.getElementById('send-message').addEventListener('click', function(){
+
+    const message = document.getElementById('input-message').value
+
+    localStorage.setItem("Message", message)
+
+})
+
+// Delete Functionality
+
+document.getElementById('delete-name').addEventListener('click', function(){
+
+    localStorage.removeItem("Name")
+
+})
+
+document.getElementById('delete-email').addEventListener('click', function(){
+
+    localStorage.removeItem("Email")
+
+})
+
+document.getElementById('delete-message').addEventListener('click', function(){
+
+    localStorage.removeItem("Message")
+
+})
