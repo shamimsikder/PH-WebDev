1e2f8d62accf8a01e3e373612d63ce59e0774d2d
shamimsikder <shamimsikder138@gmail.com>
2023-03-17 00:55:53 +0600

    localStorage

--- Milestone 7/Local Storage Practice/practice5.js
+++ Milestone 7/Local Storage Practice/practice5.js
@@ -14,3 +14,42 @@ const addProduct = () => {
     quantityField.value = ''
 
 }
+
+const getProduct = () => {
+
+    let items = {}
+    const item = localStorage.getItem('items')
+
+    if(item){
+
+        items = JSON.parse(item)
+
+    }
+
+    return items
+
+}
+
+const addItemToLocalStorage = (product, quantity) => {
+
+    const items = getProduct()
+
+    items[product] =quantity
+
+    localStorage.setItem('items', JSON.stringify(items))
+
+}
+
+const showLocalStorage = () => {
+
+    const items = getProduct()
+
+    for(const item in items){
+
+        document.getElementById('list').innerHTML += `<li>${item}: ${items[item]}`
+
+    }
+
+}
+
+showLocalStorage()
