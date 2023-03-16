7688cf610391a6a1cd9c8f2e2a3098acb585f949
shamimsikder <shamimsikder138@gmail.com>
2023-03-16 23:49:03 +0600

    getItem from localStorage

--- Milestone 7/Local Storage Practice/practice4.js
+++ Milestone 7/Local Storage Practice/practice4.js
@@ -6,9 +6,30 @@ const addProduct = () => {
     const product = productField.value
     const quantity = quantityField.value
 
+    addItemToLocalStorage(product, quantity)
+
     document.getElementById('list').innerHTML += `<li>${product}: ${quantity}`
 
     productField.value = ''
     quantityField.value = ''
 
 }
+
+const getItemFromLocalStorage = () => {
+
+    const item = JSON.parse(localStorage.getItem('data'))
+
+    return item
+
+}
+
+const addItemToLocalStorage = (product, quantity) => {
+
+    const items = getItemFromLocalStorage() || {}
+
+    items[product] = quantity
+
+    localStorage.setItem("data", JSON.stringify(items))
+
+}
+
