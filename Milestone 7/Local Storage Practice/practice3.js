16192b71efa103834fce23d858abe1c9e2996c5b
shamimsikder <shamimsikder138@gmail.com>
2023-03-16 22:19:04 +0600

    setItem in localStorage

--- Milestone 7/Local Storage Practice/practice3.js
+++ Milestone 7/Local Storage Practice/practice3.js
@@ -1,11 +1,30 @@
+const items = []
+
 const addProduct = () => {
 
-    const product = document.getElementById('product').value
-    const quantity = document.getElementById('quantity').value
+    const productField = document.getElementById('product')
+    const quantityField = document.getElementById('quantity')
+
+    const product = productField.value
+    const quantity = quantityField.value
+
+    saveToLocalStorage(product, quantity)
 
     document.getElementById('list').innerHTML += `<li>${product}: ${quantity}`
 
-    product = ''
-    quantity = ''
+    productField.value = ''
+    quantityField.value = ''
+
+}
+
+const saveToLocalStorage = (product, quantity) => {
+
+    const item = {}
+
+    item[product] = quantity
+
+    items.push(item)
+
+    localStorage.setItem("items", JSON.stringify(items))
 
 }
