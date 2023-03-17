3f2e57b7135795b302725d3c0c72ff7b80735d19
shamimsikder <shamimsikder138@gmail.com>
2023-03-17 18:53:18 +0600

    fixed date issue

--- Milestone 8/API Recap/js/script.js
+++ Milestone 8/API Recap/js/script.js
@@ -13,9 +13,17 @@ const showData = (data) => {
 
     const {login,avatar_url,name,company,blog,location,bio,twitter_username,public_repos,followers,following,created_at} = data
 
+    const date = new Date(created_at);
+    const year = date.getFullYear();
+    const month = new Intl.DateTimeFormat('en', { month: 'long' }).format(date);
+    const day = date.getDate();
+
+   // const formattedDate = `${day} ${month} ${year}`;
+
+
     document.getElementById('name').innerText = name
     document.getElementById('userName').innerText = login
-    document.getElementById('date').innerText = created_at
+    document.getElementById('date').innerText = `${day} ${month} ${year}`
     document.getElementById('bio').innerText = bio
     document.getElementById('repo').innerText = public_repos
     document.getElementById('follower').innerText = followers
@@ -24,6 +32,7 @@ const showData = (data) => {
     document.getElementById('your-link').innerText = blog
     document.getElementById('twitter').innerText = twitter_username ? twitter_username : "No Data"
     document.getElementById('company').innerText = company ? company : "No Data"
+
 
 }
 
