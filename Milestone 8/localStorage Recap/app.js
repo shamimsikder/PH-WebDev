5d26025272109ca789b485b5acff032c4c8ef75d
shamimsikder <shamimsikder138@gmail.com>
2023-03-18 18:57:21 +0600

    Complete

--- Milestone 8/localStorage Recap/app.js
+++ Milestone 8/localStorage Recap/app.js
@@ -9,12 +9,21 @@ const displayProduct = (data) => {
   data.forEach((product) => {
     const card = document.createElement("div");
     card.classList.add("card", "m-2");
-    
+    const isBookmarked = checkBookmark(product.id);
+    console.log(isBookmarked);
+
     card.innerHTML = `
           <div class="bookmark-icon">
         
-          <i  class="fa-regular fa-bookmark"></i>
-          <i class="fa-solid fa-bookmark"></i>
+          <i onclick="${
+            isBookmarked
+              ? `handleRemoveBookmark('${product.id}')`
+              : `handleBookmark('${product.name}','${product.id}','${product.price}')`
+          }" 
+           class="${
+             isBookmarked ? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"
+           }"></i>
+
 
         </div>
         <div class="product-img-container">
@@ -35,5 +44,47 @@ const displayProduct = (data) => {
   });
 };
 
+// ! handle book mark
+
+const handleBookmark = (name, id, price) => {
+  const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
+  let bookmark = [];
+  const product = { name, id, price, bookmark: true };
+
+  if (previousBookmark) {
+    const isThisProductMarked = previousBookmark.find((pd) => pd.id == id);
+    if (isThisProductMarked) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "already bokmakred",
+        footer: '<a href="">Why do I have this issue?</a>',
+      });
+    } else {
+      bookmark.push(...previousBookmark, product);
+      localStorage.setItem("bookmark", JSON.stringify(bookmark));
+      console.log(bookmark);
+    }
+  } else {
+    bookmark.push(product);
+    localStorage.setItem("bookmark", JSON.stringify(bookmark));
+  }
+};
+const handleRemoveBookmark = (id) => {
+  const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
+  const restOfThem = previousBookmark.filter((product) => product.id != id);
+  localStorage.setItem("bookmark", JSON.stringify(restOfThem));
+};
+
+const checkBookmark = (id) => {
+  const previousBookmark = JSON.parse(localStorage.getItem("bookmark"));
+
+  const isBookmarked = previousBookmark?.find((product) => product.id == id);
+  if (isBookmarked) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 loadProduct();
