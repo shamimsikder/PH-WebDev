10e368f3b17bcd906269e616e302e3b7ea74a32b
shamimsikder <shamimsikder138@gmail.com>
2023-03-20 21:48:06 +0600

    Object Values & Keys

---
+++ Milestone 8/ES6 Recap/object-values-keys.js
@@ -0,0 +1,14 @@
+const person = {
+
+    firstName : "John",
+    lastName : "Doe",
+    age : 50,
+    eyeColor : "blue"
+
+}
+
+const key = Object.keys(person)
+const value = Object.values(person)
+
+console.log(key)
+console.log(value)
