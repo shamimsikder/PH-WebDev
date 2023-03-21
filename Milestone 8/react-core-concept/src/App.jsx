96c44dd6fba5c599a011aad5dfe65258b5642a09
shamimsikder <shamimsikder138@gmail.com>
2023-03-21 11:24:01 +0600

    initial commit

--- Milestone 8/react-core-concept/src/App.jsx
+++ Milestone 8/react-core-concept/src/App.jsx
@@ -6,42 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-const x = 87
-
-const musicS = {
-
-  color: 'red'
-
-}
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>JSX</h1>
-      <div className='container'>
-        <p>{x+3}</p>
-      </div>
-      <div className="music">
-        <p style={musicS}>React Developer</p>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p style={musicS}>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
     </div>
   )
 }
