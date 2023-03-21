8fe5659f85fe4f16b8e83d97dbfe7468fd1e6a2e
shamimsikder <shamimsikder138@gmail.com>
2023-03-21 11:17:20 +0600

    initial commit

--- Milestone 8/react-core-concept/src/App.jsx
+++ Milestone 8/react-core-concept/src/App.jsx
@@ -6,6 +6,14 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+const x = 87
+
+const musicS = {
+
+  color: 'red'
+
+}
+
   return (
     <div className="App">
       <div>
@@ -16,12 +24,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>JSX</h1>
+      <div className='container'>
+        <p>{x+3}</p>
+      </div>
+      <div className="music">
+        <p style={musicS}>React Developer</p>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p style={musicS}>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
