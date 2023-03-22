88198ba56454724fc699dc765ad1f05f9178c96e
shamimsikder <shamimsikder138@gmail.com>
2023-03-23 01:25:59 +0600

    Total Countries

--- Milestone 8/rest-countries/src/App.jsx
+++ Milestone 8/rest-countries/src/App.jsx
@@ -7,8 +7,32 @@ function App() {
 
     <div className="App">
       
+      <Countries></Countries>
+
     </div>
   )
+}
+
+function Countries(){
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+
+  }, [])
+
+  return(
+
+    <div>
+      <h2>Total Countries: {countries.length}</h2>
+    </div>
+
+  )
+
 }
 
 export default App
