059fc21c4f5f4e266badfb96e7669ce730f8858c
shamimsikder <shamimsikder138@gmail.com>
2023-03-22 13:02:06 +0600

    Component & Prop

--- Milestone 8/react-core-concept3/src/App.jsx
+++ Milestone 8/react-core-concept3/src/App.jsx
@@ -4,12 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+
+  const person = [
+
+    {name: 'Ricardo Kaka', job: 'Footballer'},
+    {name: 'Tamim Iqbal', job: 'Cricketer'},
+    {name: 'Rafael Nadal', job: 'Tennis Player'}
+
+  ]
+
   return (
+
     <div className="App">
       
+      {
+        person.map(p => <People name={p.name} job={p.job}></People>)
+      }
+
     </div>
   )
+}
+
+function People(props){
+
+  return(
+
+    <div className='people'>
+      <h2>Name: {props.name}</h2>
+      <p>Job: {props.job}</p>
+    </div>
+
+  )
+
 }
 
 export default App
