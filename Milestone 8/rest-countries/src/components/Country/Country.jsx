85b8f158d3b495d77ab9f40156776dab14c8b3f4
shamimsikder <shamimsikder138@gmail.com>
2023-03-23 13:46:32 +0600

    Destructuring

--- Milestone 8/rest-countries/src/components/Country/Country.jsx
+++ Milestone 8/rest-countries/src/components/Country/Country.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+
+    const {name,area,population} = props.data
+
     return (
         <div className='countries'>
-            <h4>Country Name: {props.data.name.common}</h4>
-            <h5>Area: {props.data.area}</h5>
-            <h5>Population: {props.data.population}</h5>
+            <h4>Country Name: {name.common}</h4>
+            <h5>Area: {area}</h5>
+            <h5>Population: {population}</h5>
         </div>
     );
 };
