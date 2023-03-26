3a1d9f56c1d5353ca31ad43afca93d8fba09f81b
shamimsikder <shamimsikder138@gmail.com>
2023-03-26 16:31:53 +0600

    Store Multiple Data In localStorage

--- Milestone 8/react-handler-and-storage2/src/utilities/fakedb.jsx
+++ Milestone 8/react-handler-and-storage2/src/utilities/fakedb.jsx
@@ -1,19 +1,35 @@
 const addToDB = (id) => {
 
-    const quantity = localStorage.getItem(id)
+    let shoppingCart
 
+    const storedCart = localStorage.getItem('shopping-cart')
+    
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart)
+    }
+    else{
+        shoppingCart = {}
+    }
+
+    const quantity = shoppingCart[id]
+    
     if(quantity){
-
+        
         const newQuantity = parseInt(quantity) + 1
 
-        localStorage.setItem(id, newQuantity)
+        shoppingCart[id] = newQuantity
+
+        //localStorage.setItem(id, newQuantity)
 
     }
     else{
-
-        localStorage.setItem(id, 1)    
-    
+        
+        shoppingCart[id] = 1
+        //localStorage.setItem(id, 1)    
+        
     }
+    
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 
 }
 
