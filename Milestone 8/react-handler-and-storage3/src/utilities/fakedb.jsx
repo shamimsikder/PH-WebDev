4df5dbe1f52713411b3e4aafa540140b9386c59f
shamimsikder <shamimsikder138@gmail.com>
2023-03-26 21:05:20 +0600

    Remove Data From localStorage

--- Milestone 8/react-handler-and-storage3/src/utilities/fakedb.jsx
+++ Milestone 8/react-handler-and-storage3/src/utilities/fakedb.jsx
@@ -26,6 +26,21 @@ const addToDB = (id) => {
 
 const removeFromDB = (id) => {
 
+    const storedCart = localStorage.getItem('shopping-cart')
+
+    if(storedCart){
+
+        const   shoppingCart = JSON.parse(storedCart)
+
+        if(id in shoppingCart){
+
+            delete shoppingCart[id]
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
+        }
+
+    }
+
 }
 
 export {addToDB, removeFromDB}
