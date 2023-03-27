7a1425a96d7b844cc7509741c38247cddb1ab47d
shamimsikder <shamimsikder138@gmail.com>
2023-03-27 11:49:51 +0600

    Card Style

--- Milestone 8/ema-john-e-commerce/src/components/Product/Product.jsx
+++ Milestone 8/ema-john-e-commerce/src/components/Product/Product.jsx
@@ -6,18 +6,20 @@ const Product = (props) => {
     const {name, price, seller, ratings, img} = props.products
     
     return (
-        <div className='card'>
+        <div className='product'>
             
             <img src={img} alt="" />
 
-            <div>
+            <div className='product-info'>
 
-                <h3>{name}</h3>
+                <h3 className='product-name'>{name}</h3>
                 <p>Price: {price}</p>
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings}</p>
 
             </div>
+
+            <button className='btn-cart'>Add to Cart</button>
 
         </div>
     );
