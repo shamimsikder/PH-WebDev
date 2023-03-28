93bb3998a030c215e86ffcfea6523a4b6f16a3ee
shamimsikder <shamimsikder138@gmail.com>
2023-03-28 23:33:29 +0600

    Calculate Cart Price

--- Milestone 8/ema-john-e-commerce/src/components/Cart/Cart.jsx
+++ Milestone 8/ema-john-e-commerce/src/components/Cart/Cart.jsx
@@ -6,21 +6,26 @@ const Cart = (props) => {
     const cart = props.cart
 
     let total = 0
+    let totalShipping = 0
   
     for(const product of cart){
 
         total = total + product.price
+        totalShipping = totalShipping + product.shipping
 
     }
+
+    const tax = total*7/100
+    const grandTotal = total+totalShipping+tax
 
     return (
         <div className='cart'>
             <h2 className='order-summary'>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping Charge: </p>
-            <p>Tax: </p>
-            <h2>Grand Total: </h2>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h2>Grand Total: ${grandTotal.toFixed(2)}</h2>
         </div>
     );
 };
