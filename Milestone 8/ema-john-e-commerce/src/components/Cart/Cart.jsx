652d5e984bf1dc9f1d6861c2bd7e508cbdc72c2b
shamimsikder <shamimsikder138@gmail.com>
2023-03-28 23:21:09 +0600

    Calculate Cart Price

--- Milestone 8/ema-john-e-commerce/src/components/Cart/Cart.jsx
+++ Milestone 8/ema-john-e-commerce/src/components/Cart/Cart.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = (props) => {
+
+    const cart = props.cart
+
+    let total = 0
+  
+    for(const product of cart){
+
+        total = total + product.price
+
+    }
 
     return (
         <div className='cart'>
             <h2 className='order-summary'>Order Summary</h2>
-            <p>Selected Items:</p>
-            <p>Total Price:</p>
-            <p>Total Shipping Charge:</p>
-            <p>Tax:</p>
-            <h2>Grand Total:</h2>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: {total}</p>
+            <p>Total Shipping Charge: </p>
+            <p>Tax: </p>
+            <h2>Grand Total: </h2>
         </div>
     );
 };
--- Milestone 8/ema-john-e-commerce/src/components/Shop/Shop.jsx
+++ Milestone 8/ema-john-e-commerce/src/components/Shop/Shop.jsx
@@ -29,7 +29,7 @@ const Shop = () => {
             </div>
 
             <div className='cart-container'>
-                <Cart></Cart>
+                <Cart cart={cart}></Cart>
             </div>
             
         </div>
