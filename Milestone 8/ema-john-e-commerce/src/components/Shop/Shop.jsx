7f3f23c3be1d6c06c424a6475915ef07f07d081f
shamimsikder <shamimsikder138@gmail.com>
2023-03-29 12:07:13 +0600

    Load cart from local storage & find product

--- Milestone 8/ema-john-e-commerce/src/components/Shop/Shop.jsx
+++ Milestone 8/ema-john-e-commerce/src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
+import {addToDb, getShoppingCart} from '../../utilities/fakedb'
 
 const Shop = () => {
 
@@ -16,9 +17,26 @@ const Shop = () => {
 
     }, [])
 
+    useEffect(() => {
+
+        const storedCart = getShoppingCart()
+
+        for(const id in storedCart){
+
+            const addedProduct = products.find(product => product.id === id)
+
+            const quantity = storedCart[id]
+            addedProduct.quantity = quantity
+
+        }
+
+    }, [products])
+
     const addToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
+        
     }
 
     return (
