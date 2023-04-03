9b3a1525f39c38a00ffaa0dea66e4ae7972abd94
shamimsikder <shamimsikder138@gmail.com>
2023-04-04 02:52:04 +0600

    Simple React Router

---
+++ Milestone 9/react-router/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+
+        </div>
+    );
+};
+
+export default Header;
--- Milestone 9/react-router/src/main.jsx
+++ Milestone 9/react-router/src/main.jsx
@@ -7,26 +7,40 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
+
   {
     path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/about",
-    element: <h1>About</h1>,
-  },
-  {
-    path: "/Contact",
-    element: <h1>Contact</h1>,
-  },
-]);
+    element: <Home></Home>
+  }
+
+])
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//   },
+//   {
+//     path: "/about",
+//     element: <About></About>,
+//   },
+//   {
+//     path: "/Contact",
+//     element: <Contact></Contact>,
+//   },
+// ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        
+    
+    <Header></Header>
     <RouterProvider router={router} />
   
   </React.StrictMode>,
