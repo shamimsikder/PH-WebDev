1ab761dc7ada190e2c47b35c40cf260fc7a05b00
shamimsikder <shamimsikder138@gmail.com>
2023-04-04 02:00:14 +0600

    Simple React Router

--- Milestone 9/react-router/src/main.jsx
+++ Milestone 9/react-router/src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+  {
+    path: "/Contact",
+    element: <h1>Contact</h1>,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        
+    <RouterProvider router={router} />
+  
   </React.StrictMode>,
 )
