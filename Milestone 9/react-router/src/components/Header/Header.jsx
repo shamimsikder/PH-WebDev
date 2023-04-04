973b076c53a41337be4accbe928e0db750eca75c
shamimsikder <shamimsikder138@gmail.com>
2023-04-04 11:28:40 +0600

    avoid reload using link

--- Milestone 9/react-router/src/components/Header/Header.jsx
+++ Milestone 9/react-router/src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className='header'>
+        <nav className='header'>
             
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
 
-        </div>
+        </nav>
     );
 };
 
