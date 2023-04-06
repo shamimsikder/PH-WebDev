e8f7a7cd666f3f1a8d64d1904efa7f7dafa9b766
shamimsikder <shamimsikder138@gmail.com>
2023-04-07 03:46:06 +0600

    Remove Item handler and Make Data consistent across UI

--- Milestone 9/ema-john-e-commerce/src/components/Orders/Orders.jsx
+++ Milestone 9/ema-john-e-commerce/src/components/Orders/Orders.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
+import {removeFromDb} from '../../utilities/fakedb'
+
 
 const Orders = () => {
 
-    const cart = useLoaderData()
+    const savedCart = useLoaderData()
+    const [cart, setCart] = useState(savedCart)
+
+    const handleRemoveFromCart = (id) => {
+
+        const remaining = cart.filter(product => product.id !== id)
+
+        setCart(remaining)
+        removeFromDb(id)
+
+    }
 
     return (
         <div className='shop-container'>
@@ -14,7 +26,7 @@ const Orders = () => {
             <div className='review-container'>
                 
                 {
-                    cart.map(product => <ReviewItem key={product.id} product={product}></ReviewItem>)
+                    cart.map(product => <ReviewItem key={product.id} product={product} handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
                 }
 
             </div>
--- Milestone 9/ema-john-e-commerce/src/components/ReviewItem/ReviewItem.jsx
+++ Milestone 9/ema-john-e-commerce/src/components/ReviewItem/ReviewItem.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './ReviewItem.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import Orders from '../Orders/Orders';
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
 
     const {id, img, price, name, quantity} = product
 
@@ -11,14 +12,14 @@ const ReviewItem = ({product}) => {
         <div className='review-item'>
             
             <img src={img} alt="" />
-
+            
             <div className='review-details'>
                 <p className='product-title'>{name}</p>
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
 
-            <button className='btn-delete'>
+            <button onClick={() => handleRemoveFromCart(id)} className='btn-delete'>
                 <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
             </button>
 
