a6d7b2e309f6c034a004d3d4d3f4796f58dde99c
shamimsikder <shamimsikder138@gmail.com>
2023-04-08 16:55:42 +0600

    Add Navbar

--- Milestone 9/banglar-tshirt/src/main.jsx
+++ Milestone 9/banglar-tshirt/src/main.jsx
@@ -6,10 +6,41 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './components/Main/Main';
+import Home from './components/Home/Home';
+import OrderReview from './components/OrderReview/OrderReview';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      },
+      {
+        path: 'contact',
+        element: <Contact></Contact>
+      }
+    ]
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
