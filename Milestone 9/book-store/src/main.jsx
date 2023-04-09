20d01c631cb6e2a0f78b34341a63d93211ed067b
shamimsikder <shamimsikder138@gmail.com>
2023-04-09 17:26:25 +0600

    Added Components and Set-up Router

--- Milestone 9/book-store/src/main.jsx
+++ Milestone 9/book-store/src/main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Books from './components/Books/Books'
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    element:  <App />,
+    children: [
+
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: 'books',
+        element: <Books></Books>,
+        loader: () => fetch()
+      },
+      {
+        path: 'about',
+        element: <About></About>
+      }
+
+    ]
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
