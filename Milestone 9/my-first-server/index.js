25106c6c2ecab10af7fc4297c1c28520bad6b0ef
shamimsikder <shamimsikder138@gmail.com>
2023-04-14 02:27:16 +0600

    Host Data

---
+++ Milestone 9/my-first-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+
+    res.send("Welcome to Express, Hello Express!");
+
+})
+
+app.listen(port, () => {
+
+    console.log(`App listening on port ${port}`);
+
+})
