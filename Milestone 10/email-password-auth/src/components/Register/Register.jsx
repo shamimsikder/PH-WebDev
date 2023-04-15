12205d8eb39e01f2da508296a9fd0fdf3301f0b4
shamimsikder <shamimsikder138@gmail.com>
2023-04-15 14:57:29 +0600

    added form

--- Milestone 10/email-password-auth/src/components/Register/Register.jsx
+++ Milestone 10/email-password-auth/src/components/Register/Register.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault()
+
+    }
+
+    const handleEmailChange = (event) => {
+
+        setEmail(event.target.value)
+
+    }
+
+    const handlePasswordBlur = (event) => {
+
+
+
+    }
+
     return (
         <div>
+            
             <h1>This is register</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <input onChange={handleEmailChange} type="email" name='email' placeholder='Your Email' />
+                <br />
+                <input onBlur={handlePasswordBlur} type="password" name='password' placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Register" />
+                
+
+            </form>
+
         </div>
     );
 };
