d386e59eae47de0cfeb29c72731c235098258d6b
shamimsikder <shamimsikder138@gmail.com>
2023-04-18 15:41:27 +0600

    Added Login and Registration Form

--- Milestone 10/firebase-authentication/src/components/Login.jsx
+++ Milestone 10/firebase-authentication/src/components/Login.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div className='flex justify-center items-center'>
-            <h1>Login</h1>
+        <div className='flex flex-col justify-center items-center mt-20'>
+           
+            <h1 className='text-5xl font-semibold'>Please Login!</h1>
+
+           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-10">
+                <form className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                    </div>
+                    <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
+        
         </div>
     );
 };
