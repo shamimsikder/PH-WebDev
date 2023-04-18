8811f5085858d74c9cc7244651be938cae907b52
shamimsikder <shamimsikder138@gmail.com>
2023-04-18 16:01:36 +0600

    Added Handler for Login and Registration

--- Milestone 10/firebase-authentication/src/components/Login.jsx
+++ Milestone 10/firebase-authentication/src/components/Login.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const handleLogIn = event => {
+
+        event.preventDefault()
+
+        const form = event.target
+        const email = form.email.value 
+        const password = form.password.value
+
+    }
+
     return (
         <div className='flex flex-col justify-center items-center mt-20'>
            
             <h1 className='text-5xl font-semibold'>Please Login!</h1>
 
            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-10">
-                <form className="card-body">
+
+                <form onSubmit={handleLogIn} className="card-body">
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
@@ -27,6 +40,11 @@ const Login = () => {
                     <button className="btn btn-primary">Login</button>
                     </div>
                 </form>
+
+                <Link to='/register'>
+                    <button className='btn btn-link'>New To Site?</button>
+                </Link>
+
             </div>
         
         </div>
--- Milestone 10/firebase-authentication/src/components/Register.jsx
+++ Milestone 10/firebase-authentication/src/components/Register.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const handleRegistration = event => {
+
+        event.preventDefault()
+
+        const form = event.target 
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+
+    }
+
     return (
         <div className='flex flex-col justify-center items-center mt-20'>
             
             <h1 className='text-5xl font-semibold'>Register Here</h1>
 
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mt-10">
-                <form className="card-body">
+
+                <form onSubmit={handleRegistration} className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                    </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
@@ -20,13 +40,14 @@ const Register = () => {
                     </label>
                     <input type="password" name='password' placeholder="password" className="input input-bordered" required/>
                     <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        <Link to="/login" className="label-text-alt link link-hover">Already Have Account?</Link>
                     </label>
                     </div>
                     <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
+                    <button className="btn btn-primary">Register</button>
                     </div>
                 </form>
+
             </div>
 
         </div>
