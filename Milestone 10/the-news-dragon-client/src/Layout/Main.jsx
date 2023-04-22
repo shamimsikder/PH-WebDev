f12746d3baf88cf439d33cb762069e90900264fe
shamimsikder <shamimsikder138@gmail.com>
2023-04-23 00:33:23 +0600

    router setup

---
+++ Milestone 10/the-news-dragon-client/src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Pages/Shared/Header/Header';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
