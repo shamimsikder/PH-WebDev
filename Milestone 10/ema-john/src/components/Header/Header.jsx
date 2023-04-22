a4ec627261abca34994317eba82a7ea8b45153b2
shamimsikder <shamimsikder138@gmail.com>
2023-04-22 16:36:28 +0600

    Created Context API

--- Milestone 10/ema-john/src/components/Header/Header.jsx
+++ Milestone 10/ema-john/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Header = () => {
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -14,6 +16,7 @@ const Header = () => {
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Sign Up</Link>
             </div>
+            
         </nav>
     );
 };
---
+++ Milestone 10/ema-john/src/components/Provider/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext(null)
+
+const AuthProvider = ({children}) => {
+
+    const user = {displayName: "sasjjh"}
+
+    const authInfo = {
+
+        user,
+
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
