d5bc7e3dd375e162f8947f299cd4650931933f72
shamimsikder <shamimsikder138@gmail.com>
2023-04-22 15:49:05 +0600

    Added Password Validation

--- Milestone 10/ema-john/src/components/Signup/Signup.jsx
+++ Milestone 10/ema-john/src/components/Signup/Signup.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.css'
 import { Link } from "react-router-dom";
 
 
 const Signup = () => {
+
+    const [error, setError] = useState('')
+
+    const handleSignUp = event => {
+
+        event.preventDefault()
+
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        const confirm = form.confirm.value 
+
+        if(password !== confirm){
+            setError('Your Password Did Not Match!')
+            return
+        }
+
+        else if(password.length < 6){
+            setError('Password Must be 6 Character or Longer')
+            return
+        }
+
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>SignUp</h2>
             
-            <form >
+            <form onSubmit={handleSignUp}>
 
                 <div className='form-control'>
                     <label htmlFor="email">Email</label>
@@ -19,7 +43,7 @@ const Signup = () => {
                     <input type="password" name='password' placeholder='Your Password' required/>
                 </div>
                 <div className='form-control'>
-                    <label htmlFor="password">Confirm Password</label>
+                    <label htmlFor="confirm">Confirm Password</label>
                     <input type="password" name='confirm' placeholder='Confirm Your Password' required/>
                 </div>
 
@@ -28,7 +52,9 @@ const Signup = () => {
             </form>
 
             <p><small>Already Have an account? <Link to='/login'>Login</Link> </small></p>
-        
+
+            <p className='text-error'>{error}</p>
+
         </div>
     );
 };
