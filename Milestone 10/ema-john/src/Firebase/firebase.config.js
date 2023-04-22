9e0cf102f1cf6816c9189f996629504b5cd1147a
shamimsikder <shamimsikder138@gmail.com>
2023-04-22 14:22:49 +0600

    Firebase Setup

---
+++ Milestone 10/ema-john/src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxUQX0RJNmoqgsLPFtRGkCDkObHq1iXxE",
+  authDomain: "ema-john-53b72.firebaseapp.com",
+  projectId: "ema-john-53b72",
+  storageBucket: "ema-john-53b72.appspot.com",
+  messagingSenderId: "439858608532",
+  appId: "1:439858608532:web:a3b591fbe329709d057781"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
