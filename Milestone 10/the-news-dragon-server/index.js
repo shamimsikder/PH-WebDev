edb2cf577396ad1d7883e49b7cbebdbd29658184
shamimsikder <shamimsikder138@gmail.com>
2023-04-24 02:46:49 +0600

    Category data loading API

--- Milestone 10/the-news-dragon-server/index.js
+++ Milestone 10/the-news-dragon-server/index.js
@@ -19,6 +19,30 @@ app.get('/news', (req, res) => {
     res.send(news)
 })
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+
+    const id = parseInt(req.params.id)
+    
+    if(id == 0){
+        res.send(news)
+    }
+
+    else{
+
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+
+    }
+
+})
+
 app.get('/categories', (req, res) => {
 
     res.send(categories)
