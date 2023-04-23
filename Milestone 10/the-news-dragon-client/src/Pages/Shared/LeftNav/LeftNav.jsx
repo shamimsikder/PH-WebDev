5daec15d62822e896cecb68ca314e573c0e0f523
shamimsikder <shamimsikder138@gmail.com>
2023-04-24 01:27:00 +0600

    Added Left Nav

--- Milestone 10/the-news-dragon-client/src/Pages/Shared/LeftNav/LeftNav.jsx
+++ Milestone 10/the-news-dragon-client/src/Pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error))
+
+    }, [])
+
     return (
         <div>
-            <h1>Left Nav</h1>
+            <h3>All Category</h3>
+
+            <div className='ps-4'>
+
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/category/${category.id}`} className='text-black text-decoration-none'>{category.name}</Link>
+                    </p>)
+                }
+
+            </div>
+
         </div>
     );
 };
