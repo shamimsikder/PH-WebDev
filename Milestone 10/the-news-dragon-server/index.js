f74d838b351fa1a01765800d7ae052a15ccca4d6
shamimsikder <shamimsikder138@gmail.com>
2023-04-24 02:41:07 +0600

    News Data Loading API

--- Milestone 10/the-news-dragon-server/index.js
+++ Milestone 10/the-news-dragon-server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 const cors = require('cors')
 
@@ -12,6 +13,10 @@ app.get('/', (req, res) => {
 
     res.send('Dragon is running')
 
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
 })
 
 app.get('/categories', (req, res) => {
