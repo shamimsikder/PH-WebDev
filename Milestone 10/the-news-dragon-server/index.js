6670559a45aa2e6fbd385930c5ffb664fcf0e7b9
shamimsikder <shamimsikder138@gmail.com>
2023-04-24 01:14:28 +0600

    server setup

--- Milestone 10/the-news-dragon-server/index.js
+++ Milestone 10/the-news-dragon-server/index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const categories = require('./data/categories.json')
+
 app.get('/', (req, res) => {
 
     res.send('Dragon is running')
+
+})
+
+app.get('/categories', (req, res) => {
+
+    res.send(categories)
 
 })
 
