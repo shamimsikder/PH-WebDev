2305f848b512783490b886b87b8fe564be95e473
shamimsikder <shamimsikder138@gmail.com>
2023-04-23 16:45:12 +0600

    Added React Marquee

--- Milestone 10/the-news-dragon-client/src/Pages/Shared/Header/Header.jsx
+++ Milestone 10/the-news-dragon-client/src/Pages/Shared/Header/Header.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             
             <div className='text-center'>
                 <img src={logo} alt="" />
-                <p><small>Journalism Without Fear or Favour</small></p>
+                <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
 
-        </div>
+            <div className='mt-5 mb-5 d-flex'>
+                <Button variant="danger">Latest</Button>{' '}
+                <Marquee className='text-danger' speed={80}>
+                    I can be a React component, multiple React components, 
+                    or just some text......  
+                    I can be a React component, multiple React components, 
+                    or just some text
+                </Marquee>
+            </div>
+
+        </Container>
     );
 };
 
