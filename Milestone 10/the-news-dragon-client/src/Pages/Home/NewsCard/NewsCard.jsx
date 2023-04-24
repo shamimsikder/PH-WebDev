14933bfcaa266b058135a79e1d3cea8befb2ee2e
shamimsikder <shamimsikder138@gmail.com>
2023-04-24 15:57:56 +0600

    Added Cards Details

--- Milestone 10/the-news-dragon-client/src/Pages/Home/NewsCard/NewsCard.jsx
+++ Milestone 10/the-news-dragon-client/src/Pages/Home/NewsCard/NewsCard.jsx
@@ -1,6 +1,9 @@
+import moment from 'moment';
 import React from 'react';
 import { Button, Card, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
+
 
 const NewsCard = ({news}) => {
 
@@ -8,8 +11,15 @@ const NewsCard = ({news}) => {
 
     return (
         <Card className=" mb-4">
-            <Card.Header>
-                <Image src={author.img} roundedCircle />
+            <Card.Header className='d-flex align-items-center'>
+                <Image style={{height: '40px'}} src={author?.img} roundedCircle />
+                <div className='ps-2 flex-grow-1'>
+                    <p className='mb-0'>{author?.name}</p>
+                    <p><small>{moment(author?.published_date).format('yyyy-MM-D')}</small></p>
+                </div>
+                <div>
+                    <FaRegBookmark/> <FaShareAlt/>
+                </div>
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
