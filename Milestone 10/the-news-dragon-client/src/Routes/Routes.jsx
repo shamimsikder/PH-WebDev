1427110d708b7e93e180d577e87984747ed0719b
shamimsikder <shamimsikder138@gmail.com>
2023-04-25 15:01:36 +0600

    Create Login Layout for Login and Registration Route

--- Milestone 10/the-news-dragon-client/src/Routes/Routes.jsx
+++ Milestone 10/the-news-dragon-client/src/Routes/Routes.jsx
@@ -1,24 +1,44 @@
 
 import React from "react";
-import {createBrowserRouter} from "react-router-dom";
+import {Navigate, createBrowserRouter} from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Category from "../Pages/Home/Category/Category";
 import NewsLayout from "../Layout/NewsLayout";
 import News from "../Pages/News/News/News";
+import LoginLayout from "../Layout/LoginLayout";
+import Login from "../Pages/Login/Login/Login";
+import Register from "../Pages/Login/Register/Register";
   
   const router = createBrowserRouter([
+
     {
       path: "/",
-      element: <Main></Main>,
+      element: <LoginLayout></LoginLayout>,
       children: [
         {
-            path: "/",
-            element: <Category></Category>,
-            loader: () => fetch('http://localhost:5000/news')
+          path: '/',
+          element: <Navigate to="/category/0"></Navigate>
         },
         {
-          path: '/category/:id',
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
+          path: 'register',
+          element: <Register></Register>
+        }
+
+      ]
+    },
+
+    {
+      path: "category",
+      element: <Main></Main>,
+      children: [
+        
+        {
+          path: ':id',
           element: <Category></Category>,
           loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
         }
