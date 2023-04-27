117fe60e5ef863673d04989b28c8853c88ed5372
shamimsikder <shamimsikder138@gmail.com>
2023-04-27 15:37:49 +0600

    Added Header

--- Milestone 10/simple-firebase-authentication/src/Components/Header/Header.jsx
+++ Milestone 10/simple-firebase-authentication/src/Components/Header/Header.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h1>this is header</h1>
+           
+            <nav className='d-flex justify-content-around align-items-center bg-secondary p-3 flex-wrap'>
+                <div className='logo'>
+                    <img className='logo-img' src="https://i.ibb.co/TtRpKPP/doctor.png" alt="" />
+                </div>
+                <div className='menu-container  d-flex flex-wrap'>
+
+                <Link to="/home" className="text-decoration-none">
+                    <li className="nav-link items  ms-3 text-info fw-bolder">Home</li>
+                </Link>
+                <Link to="/login" className="text-decoration-none">
+                    <li className="nav-link items  ms-3 text-info fw-bolder">Login</li>
+                </Link>
+
+                <li
+                    role="button"
+                    className="nav-link items  ms-3 text-info fw-bolder"
+                >
+                    Logout
+                </li>
+
+                <Link to="/register" className="text-decoration-none">
+                    <li className="nav-link items  ms-3 text-info fw-bolder">
+                    Register
+                    </li>
+                </Link>
+
+                </div>
+            </nav>
+
         </div>
     );
 };
