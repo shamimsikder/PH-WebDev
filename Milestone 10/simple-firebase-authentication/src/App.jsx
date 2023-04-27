00a41361693e51716c628c4c8d710fda3151fdb5
shamimsikder <shamimsikder138@gmail.com>
2023-04-27 14:08:10 +0600

    Route Setup

--- Milestone 10/simple-firebase-authentication/src/App.jsx
+++ Milestone 10/simple-firebase-authentication/src/App.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  createBrowserRouter,
+  RouterProvider,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MainLayout from './Components/Layout/MainLayout';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
   
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/home",
+          element: <Home></Home>,
+        },
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+        {
+          path: "/register",
+          element: <Register></Register>,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
-      
+
+      <RouterProvider router={router}></RouterProvider>
         
     </>
   )
