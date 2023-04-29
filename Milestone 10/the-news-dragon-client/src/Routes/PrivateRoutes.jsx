6845b297788696475f2c5469f869e29881ac201a
shamimsikder <shamimsikder138@gmail.com>
2023-04-29 13:10:26 +0600

    redirect from the login page

--- Milestone 10/the-news-dragon-client/src/Routes/PrivateRoutes.jsx
+++ Milestone 10/the-news-dragon-client/src/Routes/PrivateRoutes.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProviders';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoutes = ({children}) => {
 
     const {user} = useContext(AuthContext)
+    const location = useLocation()
+    const from = location.state?.from?.pathname || '/category/0'
 
-    return (
-        <div>
-            
-        </div>
-    );
+    if(user){
+        return children
+    }
+
+    return <Navigate state={{from: location}} to="/login" replace></Navigate>
 };
 
 export default PrivateRoutes;
