555650d3e3a602bd74dcbaafab990c87bfa9f502
shamimsikder <shamimsikder138@gmail.com>
2023-05-07 23:42:14 +0600

    load and render data in ui

--- Milestone 11/users-management-client/src/App.jsx
+++ Milestone 11/users-management-client/src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+
+  }, [])
 
   return (
     <>
       
       <h1>Users Management</h1>
+      <h1>Number of Users:  {users.length}</h1>
+
+      {
+        users.map(user => <p key={user.id}>{user.name} : {user.email}</p>)
+      }
       
     </>
   )
