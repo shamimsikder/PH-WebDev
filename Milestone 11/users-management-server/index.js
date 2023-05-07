a023f569cb74f1bcfe4542e8c3efdea26c4508a1
shamimsikder <shamimsikder138@gmail.com>
2023-05-08 01:16:58 +0600

    added data in server

--- Milestone 11/users-management-server/index.js
+++ Milestone 11/users-management-server/index.js
@@ -1,30 +1,40 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const users = [
-
-    {id: 1, name: 'Kuddus', email: 'kuddus@gmail.com'},
-    {id: 2, name: 'Akkkas', email: 'akkkas@gmail.com'},
-    {id: 3, name: 'Nantu', email: 'Nantu@gmail.com'}
-    
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com' },
+    { id: 2, name: 'Sabnoon', email: 'sabnoon@gmail.com' },
+    { id: 3, name: 'Sabila', email: 'sabila@gmail.com' },
 ]
 
 app.get('/', (req, res) => {
 
-    res.send("This is users server")
+    res.send('Users Management server is running')
+    
+});
+
+
+app.get('/users', (req, res) =>{
+
+    res.send(users);
 
 })
 
-app.get('/users', (req, res) => {
-
-    res.send(users)
+app.post('/users', (req, res) =>{
+   
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 
 })
 
 app.listen(port, () => {
-    console.log(`server is running on PORT: ${port}`)
+    console.log(`Server is running on PORT: ${port}`)
 })
