2be52b596a72f03b9c0f1dc4ad13b4d83f373986
shamimsikder <shamimsikder138@gmail.com>
2023-05-07 23:40:28 +0600

    added midelware

--- Milestone 11/users-management-server/index.js
+++ Milestone 11/users-management-server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-// const cors = require('cors')
+const cors = require('cors')
 
-// app.use(cors)
+app.use(cors())
 
 const users = [
 
