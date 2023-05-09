4829b5eabb94df0c96f956457307b716d64a913a
shamimsikder <shamimsikder138@gmail.com>
2023-05-10 00:33:58 +0600

    delete data from database

--- Milestone 11/simple-crud-server/index.js
+++ Milestone 11/simple-crud-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -42,6 +42,16 @@ const client = new MongoClient(uri, {
           res.send(result);
       
         });
+
+        app.delete('/users/:id', async(req, res) => {
+
+          const id = req.params.id;
+          console.log('please delete from database', id)
+          const query = {_id: new ObjectId(id)}
+          const result = await userCollection.deleteOne(query)
+          res.send(result)
+
+        })
   
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
