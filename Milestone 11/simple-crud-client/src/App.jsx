c26fb999af972e2343a15ded073ddde6f715ea61
shamimsikder <shamimsikder138@gmail.com>
2023-05-09 16:14:28 +0600

    added data form and insert to database

--- Milestone 11/simple-crud-client/src/App.jsx
+++ Milestone 11/simple-crud-client/src/App.jsx
@@ -1,10 +1,47 @@
+import React from 'react'
+
 import './App.css'
 
 function App() {
   
+  const handleUser = event => {
+
+      event.preventDefault()
+
+      const form = event.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const user = { name, email }
+
+      fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })
+
+  }
+
   return (
     <>
+      
       <h1>Simple CRUD</h1>
+
+      <form onSubmit={handleUser}>
+
+        <input type="text" name="name" id="name" />
+        <br />
+        <input type="email" name="email" id="email" />
+        <br />
+        <input type="submit" name="" value="Add User" id="" />
+
+      </form>
+
     </>
   )
 }
