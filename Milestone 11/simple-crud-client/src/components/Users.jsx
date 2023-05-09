b23446bba9199a4599e861218e39e9cf32f4628e
shamimsikder <shamimsikder138@gmail.com>
2023-05-10 00:34:48 +0600

    delete data

--- Milestone 11/simple-crud-client/src/components/Users.jsx
+++ Milestone 11/simple-crud-client/src/components/Users.jsx
@@ -4,11 +4,30 @@ import { useLoaderData } from 'react-router-dom';
 const Users = () => {
     
     const users = useLoaderData()
+
+    const handleDelete = (id) => {
+
+        console.log(id)
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+
+            if(data.deleteCount > 0){
+                alert('Delete Successful')
+            }
+
+        })
+
+    }
     
     return (
         <div>
             <h1>{users.length}</h1>
-            {users.map(user => <p key={user.id}>{user.name} : {user.email}</p>)}
+            <div>
+                {users.map(user => <p key={user._id}>{user.name} : {user.email} <button onClick={() => handleDelete(user._id)}>X</button></p>)}
+            </div>
         </div>
     );
 };
