249ecbbf9498ccb577f985bbc732bff0d807627a
shamimsikder <shamimsikder138@gmail.com>
2023-05-14 03:02:11 +0600

    added login form

--- Milestone 11/car-doctor-client/src/Routes/Routes.jsx
+++ Milestone 11/car-doctor-client/src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -13,7 +14,11 @@ const router = createBrowserRouter([
         {
             path: "/",
             element: <Home></Home>
-        }
+        },
+        {
+            path: "login",
+            element: <Login></Login>
+        },
 
       ]
     },
