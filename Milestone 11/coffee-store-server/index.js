ef8a3f085baa835f4fb5e1b2b940eadf9627590a
shamimsikder <shamimsikder138@gmail.com>
2023-05-13 15:37:25 +0600

    Get Data by ID

--- Milestone 11/coffee-store-server/index.js
+++ Milestone 11/coffee-store-server/index.js
@@ -40,6 +40,16 @@ async function run() {
 
     })
 
+    app.get('/coffee/:id', async(req, res) => {
+
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await coffeeCollection.findOne(query)
+
+        res.send(result)
+
+    })
+
     app.post('/coffee', async(req, res) => {
 
         const newCoffee = req.body
