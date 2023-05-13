a4f3bfe4ab6872d6d8ec77ac80481e395827295c
shamimsikder <shamimsikder138@gmail.com>
2023-05-13 15:52:05 +0600

    Update single coffee information

--- Milestone 11/coffee-store-server/index.js
+++ Milestone 11/coffee-store-server/index.js
@@ -58,6 +58,31 @@ async function run() {
     
     })
 
+    app.put('/coffee/:id', async(req, res) => {
+
+        const id = req.params.id
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedCoffee = req.body;
+
+        const coffee = {
+            $set: {
+                name: updatedCoffee.name, 
+                quantity: updatedCoffee.quantity, 
+                supplier: updatedCoffee.supplier, 
+                taste: updatedCoffee.taste, 
+                category: updatedCoffee.category, 
+                details: updatedCoffee.details, 
+                photo: updatedCoffee.photo
+            }
+        }
+
+        const result = await coffeeCollection.updateOne(filter, coffee, options);
+        res.send(result);
+
+
+    })
+
     app.delete('/coffee/:id', async(req, res) => {
 
         const id = req.params.id
