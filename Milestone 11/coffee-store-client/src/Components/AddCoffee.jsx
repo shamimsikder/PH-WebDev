2b539665164f8f81b106fe241edfddcdd33e86c5
shamimsikder <shamimsikder138@gmail.com>
2023-05-13 13:52:31 +0600

    Send Coffee data to the server

--- Milestone 11/coffee-store-client/src/Components/AddCoffee.jsx
+++ Milestone 11/coffee-store-client/src/Components/AddCoffee.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const AddCoffee = () => {
 
-    const handleAddCoffee = () => {
-        
+    const handleAddCoffee = (event) => {
+
+        event.preventDefault()
+
+        const form = event.target
+        const name = form.name.value 
+        const quantity = form.quantity.value
+        const supplier = form.supplier.value
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
     }
 
     return (
