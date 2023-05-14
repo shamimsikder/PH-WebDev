d74541b3f52aeaac8d00d8f86a03756bc4a03667
shamimsikder <shamimsikder138@gmail.com>
2023-05-14 12:44:55 +0600

    added create user

--- Milestone 11/car-doctor-client/src/Pages/SignUp/SignUp.jsx
+++ Milestone 11/car-doctor-client/src/Pages/SignUp/SignUp.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import img from '../../assets/images/login/login.svg'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const SignUp = () => {
 
+    const {createUser} = useContext(AuthContext)
+
     const handleSignUp = event => {
+
+        event.preventDefault()
+
+        const form = event.target
+
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user
+            })
+            .then(error => console.log(error))
 
     }
 
--- Milestone 11/car-doctor-client/src/Providers/AuthProvider.jsx
+++ Milestone 11/car-doctor-client/src/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import {getAuth} from 'firebase/auth'
+import React, { createContext, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const  AuthContext = createContext()
@@ -7,9 +7,20 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+
+        return createUserWithEmailAndPassword(auth, email, password)
+
+    }
+
     const authInfo = {
 
-    
+        user,
+        loading,
+        createUser,
 
     }
     
