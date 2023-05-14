61af594a76cfbd20ac9e30b36a0631641f01a8fa
shamimsikder <shamimsikder138@gmail.com>
2023-05-15 01:37:27 +0600

    added book page

--- Milestone 11/car-doctor-client/src/Pages/Shared/Navbar.jsx
+++ Milestone 11/car-doctor-client/src/Pages/Shared/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
     const navItems = <>
         <li><Link to="/">Home</Link> </li>
         <li> <Link to="/about">About</Link> </li>
-        <li> <Link to="/checkout">Checkout</Link> </li>
     </>
 
     return (
--- Milestone 11/car-doctor-client/src/Routes/Routes.jsx
+++ Milestone 11/car-doctor-client/src/Routes/Routes.jsx
@@ -5,7 +5,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
-import Checkout from "../Pages/Checkout/Checkout";
+import BookService from "../Pages/BookService/BookService";
 
 const router = createBrowserRouter([
     {
@@ -26,8 +26,9 @@ const router = createBrowserRouter([
             element: <SignUp></SignUp>
         },
         {
-            path: "checkout",
-            element: <Checkout></Checkout>
+          path: 'book/:id', 
+          element: <BookService></BookService>, 
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
         },
 
       ]
