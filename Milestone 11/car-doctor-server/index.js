9f0846cb323d405b02138f39ee47efa47b833e0f
shamimsikder <shamimsikder138@gmail.com>
2023-05-15 01:37:02 +0600

    get data by id

--- Milestone 11/car-doctor-server/index.js
+++ Milestone 11/car-doctor-server/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express =  require('express')
 const cors = require('cors')
 require('dotenv').config()
@@ -26,6 +26,7 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -37,6 +38,17 @@ async function run() {
 
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
+        res.send(result)
+
+    })
+
+    app.get('/services/:id', async(req, res) => {
+
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const options = {projection: {title: 1, price: 1, service_id: 1}}
+
+        const result = await serviceCollection.findOne(query, options)
         res.send(result)
 
     })
