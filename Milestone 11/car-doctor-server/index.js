56a7c9ce6a63f7415e7dc8e8ba4e29aec2c3910c
shamimsikder <shamimsikder138@gmail.com>
2023-05-15 14:51:21 +0600

    get data by query

--- Milestone 11/car-doctor-server/index.js
+++ Milestone 11/car-doctor-server/index.js
@@ -65,8 +65,11 @@ async function run() {
 
     app.get('/bookings', async(req, res) => {
 
-      const cursor = bookingCollection.find()
-      const result = await cursor.toArray()
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
       res.send(result)
 
   })
