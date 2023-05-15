8e723be7400c0eee58061b4af42f5ae34095f7db
shamimsikder <shamimsikder138@gmail.com>
2023-05-15 16:45:13 +0600

    added bookings card

--- Milestone 11/car-doctor-client/src/Pages/Bookings/Bookings.jsx
+++ Milestone 11/car-doctor-client/src/Pages/Bookings/Bookings.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import BookingsRow from './BookingsRow';
 
 const Bookings = () => {
 
     const {user} = useContext(AuthContext)
+    const [bookings, setBookings] = useState([])
 
     const url = `http://localhost:5000/bookings?email=${user.email}`
 
@@ -11,13 +13,51 @@ const Bookings = () => {
 
         fetch(url)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setBookings(data))
 
     }, [])
 
+    const handleDelete = id => {
+
+    }
+
+    const handleBookingConfirm = id => {
+
+    }
+
     return (
         <div>
-            
+            <h2 className="text-5xl">Your bookings: {bookings.length}</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bookings.map(booking => <BookingsRow
+                                key={booking._id}
+                                booking={booking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
+                            ></BookingsRow>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
