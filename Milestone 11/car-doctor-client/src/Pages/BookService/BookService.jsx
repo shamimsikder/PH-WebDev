e7794771375bf557cae78c4a57926d3d9586cebe
shamimsikder <shamimsikder138@gmail.com>
2023-05-15 12:57:21 +0600

    send data to api and db

--- Milestone 11/car-doctor-client/src/Pages/BookService/BookService.jsx
+++ Milestone 11/car-doctor-client/src/Pages/BookService/BookService.jsx
@@ -12,6 +12,7 @@ const BookService = () => {
 
         event.preventDefault();
 
+        const form = event.target;
         const name = form.name.value;
         const date = form.date.value;
         const email = user?.email;
@@ -29,18 +30,19 @@ const BookService = () => {
         }
 
         fetch('http://localhost:5000/bookings', {
-            method: 'POST',
+            method: 'POST', 
             headers: {
                 'content-type': 'application/json'
-            },
+            }, 
             body: JSON.stringify(booking)
         })
-            .then(res => res.json())
-            .then(data => {
-                if(data.insertedId){
-                    alert('service book successfully')
-                }
-            })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('service book successfully')
+            }
+        })
 
     }
 
