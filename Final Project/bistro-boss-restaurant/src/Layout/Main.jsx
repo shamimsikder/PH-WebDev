e01998eb48874f9510efceff134269f0262ebd7f
shamimsikder <shamimsikder138@gmail.com>
2023-05-23 17:04:36 +0600

    navbar

--- Final Project/bistro-boss-restaurant/src/Layout/Main.jsx
+++ Final Project/bistro-boss-restaurant/src/Layout/Main.jsx
@@ -6,7 +6,9 @@ const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-24vh)]'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
