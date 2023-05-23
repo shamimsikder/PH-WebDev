4ebeb2e8f2a02fddf1f59abd1ec7d7829886843d
shamimsikder <shamimsikder138@gmail.com>
2023-05-23 22:20:59 +0600

    menu items section

--- Final Project/bistro-boss-restaurant/src/Pages/Home/Home.jsx
+++ Final Project/bistro-boss-restaurant/src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner";
 import Category from "./Category";
+import PopularMenu from "./PopularMenu";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div className="">
             <Banner></Banner>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
