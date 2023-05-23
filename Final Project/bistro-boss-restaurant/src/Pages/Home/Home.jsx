3839bf4d3dec8dc6fbec715762aab7d970f518b4
shamimsikder <shamimsikder138@gmail.com>
2023-05-23 22:55:52 +0600

    testimonials section

--- Final Project/bistro-boss-restaurant/src/Pages/Home/Home.jsx
+++ Final Project/bistro-boss-restaurant/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "./Banner";
 import Category from "./Category";
 import Featured from "./Featured/Featured";
 import PopularMenu from "./PopularMenu";
+import Testimonials from "./Testimonials";
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <Category></Category>
             <PopularMenu></PopularMenu>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
