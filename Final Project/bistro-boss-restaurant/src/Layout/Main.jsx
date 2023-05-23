420c18d70eb60be220ee8ab9b98d976dd374a9b4
shamimsikder <shamimsikder138@gmail.com>
2023-05-23 16:48:45 +0600

    footer

--- Final Project/bistro-boss-restaurant/src/Layout/Main.jsx
+++ Final Project/bistro-boss-restaurant/src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
