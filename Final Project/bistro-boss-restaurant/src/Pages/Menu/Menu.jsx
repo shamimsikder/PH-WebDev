11bd2bb8446fa7511d6df8068f48b1dfc7a063f3
shamimsikder <shamimsikder138@gmail.com>
2023-05-25 12:50:44 +0600

    menu page

--- Final Project/bistro-boss-restaurant/src/Pages/Menu/Menu.jsx
+++ Final Project/bistro-boss-restaurant/src/Pages/Menu/Menu.jsx
@@ -1,15 +1,39 @@
 import { Helmet } from 'react-helmet-async';
-import menuImg from '../../assets/menu/menu-bg.png'
+import menuImg from '../../assets/menu/banner3.jpg'
 import Cover from './Cover';
+import soupImg from '../../assets/menu/soup-bg.jpg'
+import saladImg from '../../assets/menu/salad-bg.jpg'
+import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+import useMenu from '../../Hooks/useMenu';
+import MenuCategory from './MenuCategory';
+import SectionTitle from '../../Components/SelectionTitle/SelectionTitle';
 
 
 const Menu = () => {
+
+    const [menu] = useMenu()
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+       
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="our menu"></Cover>
+            {/* main cover */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* dessert menu items  */}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
         </div>
     );
 };
