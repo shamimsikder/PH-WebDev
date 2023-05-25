5552472181610db676269ac43b210c4704198791
shamimsikder <shamimsikder138@gmail.com>
2023-05-25 11:40:23 +0600

    added cover

--- Final Project/bistro-boss-restaurant/src/Pages/Menu/Menu.jsx
+++ Final Project/bistro-boss-restaurant/src/Pages/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from 'react-helmet-async';
+import menuImg from '../../assets/menu/menu-bg.png'
+import Cover from './Cover';
 
 
 const Menu = () => {
@@ -7,6 +9,7 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
         </div>
     );
 };
